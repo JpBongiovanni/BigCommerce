93dc3a64554b7eed99b49e51d7d5d2c0de95bf50
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-08-26 13:02:04 -0400

    first commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -96,3 +96,17 @@ window.stencilBootstrap = function stencilBootstrap(pageType, contextJSON = null
         },
     };
 };
+
+$('productView-images').hover(
+    function() {
+        var image = $(this).find('.card-image');
+        var newImg = image.attr('data-hoverimage');
+        var currentImg = image.attr('src');
+        if (newImg && newImg != '') image.attr('src', newImg);
+    }, function() {
+        var image = $(this).find('.card-image');
+        var newImg = image.attr('data-src');
+        var currentImg = image.attr('src');
+        if (newImg && newImg != '') image.attr('src', newImg);
+    }
+);
